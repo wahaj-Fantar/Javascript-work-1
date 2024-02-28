8d523de5d2d109c2bc49a1f8624078ae32b97dd3
wahaj-Fantar <wahaj1097@gmail.com>
2024-02-28 23:59:33 +0500

    javascript 1

---
+++ script.js
@@ -0,0 +1,87 @@
+//---Chapter 1 ---
+
+//---------Q1---------
+//alert("Error! Please enter a valid password.");
+
+//---------Q2---------
+//alert("Welcome to JS Land... \n Happy Coding!");
+
+
+//---------Q3---------
+//alert("Welcome to JS Land... ");
+//alert("Happy Coding!");
+
+
+//--------Q4----------
+//console.log("Hello... I can run JS through my web browser's console");
+
+//---Chapter 2---
+
+//---------Q1---------
+//var username;
+
+//---------Q2---------
+//var myName = "Muhammad Wahaj";
+
+//--------Q3---------
+//var message = "Hello World";
+//alert(message);
+
+//--------Q4--------
+//var Name = "John Doe"
+//var Age = 15;
+//var Course = "Certified Mobile Application Development"
+//alert("Name is "+ Name);
+//alert("Age is "+ Age);
+//alert("Course is "+ Course);
+
+//-------Q5---------
+//var fastFood = " PIZZA \n PIZZ \n PIZ \n PI \n P";
+//alert(fastFood);
+
+//-------Q6---------
+//var email = "wahaj1097@gmail.com";
+//alert("My Email Address is " + email);
+
+//------Q7---------
+//var book = "A smarter way to learn JavaScript.";
+//alert("I am trying to learn from the Book "+ book);
+
+//------Q8---------
+//document.write("<h1>Yah! I can write HTML content through Javascript.");
+
+//------Q9---------
+//var style = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+//alert(style);
+
+//---Chapter 3---
+
+//----Q1-----
+//var age = 20;
+//alert("I am " + age + " years old.");
+
+//----Q2-----
+//var visited;
+//visited = "you have visited this site 14 times.";
+//alert(visited);
+
+
+//----Q3-----
+//var birthYear = 2003;
+//document.write("My birth year is " + birthYear + ".<br>Data type of my declared variable is number");
+
+//---Q5------
+//alert("Welcome to XYZ Clothing store")
+//var visitorName = prompt("Enter your name:");
+//var productTitle = prompt("Enter your product:");
+//var quantity = prompt("Enter your quantity");
+//alert(name + " ordered " + quantity + " " + productTitle + " on XYZ Clothing store.");
+
+
+
+
+
+
+
+
+
